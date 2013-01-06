29d7b88ee74e9a003c4cd593056d3b981fe26bbe
Dan Griffin <dgriff1@gmail.com>
2013-01-05 18:40:06 -0700

    turn it into a compojure app

--- src/clj_mmo/core.clj
+++ src/clj_mmo/core.clj
@@ -1,6 +1,25 @@
-(ns clj-mmo.core)
+(ns clj-mmo.core
+ (:use compojure.core [ring.adapter.jetty :only [run-jetty]] [clojure.walk :only [keywordize-keys]] )
+ (:require [compojure.route :as route]
+	[monger core util]
+	[ring.util.response :as resp]
+	[compojure.handler :as handler])
+)
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+(defroutes main-routes
+  	; what's going on
+	; fall backs 
+	(GET "/" [] (resp/redirect "/index.html"))
+	(route/resources "/")
+	(route/not-found "Page not found")   
+)
+
+(prn "Connecting to " (System/getenv "MONGOLAB_URI"))
+(prn (monger.core/connect-via-uri! (System/getenv "MONGOLAB_URI" ) ))
+
+(def app
+  (handler/api main-routes))
+
+(defn -main [port]
+	(run-jetty app {:port (Integer. port)}))
+
