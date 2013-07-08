164162f4b158cdc22f685c8a80425a52e4a09635
mike <mike@mittmiles.(none)>
2013-07-07 19:40:21 -0500

    test for calc player pos

--- resources/public/js/test/test.js
+++ resources/public/js/test/test.js
@@ -6,6 +6,9 @@ playerStruct = {  '_id' : 1,
             'location' : {'x' : 1, 'y' : 1}
 }
 
+ourGame.realPlayerCoords['x'] = 100;
+ourGame.realPlayerCoords['y'] = 50;
+
 test( "Width settr check", function() {
   ok( ourGame.width != undefined, "Passed!" );
 });
@@ -20,6 +23,9 @@ test( "init buffer check", function() {
   ok( ourGame.buffer._id == 1, "Passed!" );
 });
 
-
+test( "calculate other players position", function() {
+  ok( ourGame.calculatePosition(5, 5, 30, 95)[0] == 110, "Passed!" );
+  ok( ourGame.calculatePosition(5, 5, 30, 95)[1] == -62.5, "Passed!" );
+});
 
 
