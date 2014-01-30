22bb498ba27eb4843ba92b2dd8ed5f45d2dce371
justmike2000 <justmike2000@gmail.com>
2014-01-30 13:49:04 -0600

    Fixed tests

--- resources/public/js/test/test.js
+++ resources/public/js/test/test.js
@@ -6,8 +6,8 @@ playerStruct = {  '_id' : 1,
                   'location' : {'x' : 1, 'y' : 1}
 }
 
-ourGame.realPlayerCoords['x'] = 100;
-ourGame.realPlayerCoords['y'] = 50;
+ourGame.playerGameCoords['x'] = 100;
+ourGame.playerGameCoords['y'] = 50;
 
 test( "Width settr check", function() {
   ok( ourGame.width != undefined, "Passed!" );
@@ -24,8 +24,9 @@ test( "init buffer check", function() {
 });
 
 test( "calculate other players position", function() {
-  ok( ourGame.calculatePosition(5, 5, 30, 95)[0] == 110, "Passed!" );
-  ok( ourGame.calculatePosition(5, 5, 30, 95)[1] == -62.5, "Passed!" );
+  pos =  ourGame.calculatePosition(5, 5, 30, 95);	
+  ok( ourGame.calculatePosition(5, 5, 30, 95)[0] == 75, "Passed!" );
+  ok( ourGame.calculatePosition(5, 5, 30, 95)[1] == -40, "Passed!" );
 });
 
 
