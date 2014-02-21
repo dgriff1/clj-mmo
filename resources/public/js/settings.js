8ffb5cef417e145843d4ec344db3b1d2537554b9
justmike2000 <justmike2000@gmail.com>
2014-02-21 15:00:55 -0600

    movement speed chnge

---
+++ resources/public/js/settings.js
@@ -0,0 +1,31 @@
+
+function _settings()
+{
+	window.Settings = this;
+	var self = this;
+
+	self.BASE_WIDTH = 800;
+	self.BASE_HEIGHT = 350;
+
+//GRAPHIC
+	self.FPS_RATE = 60;
+//WORLD
+	self.MOVEMENT_SPEED = 45;
+	self.MAX_MOVEMENT_SPEED = self.MOVEMENT_SPEED + 0.04;
+// NETWORK
+	// Increase for smoother updates but lowers performance
+	self.UPDATE_RATE = 0.020;
+
+	// New Map Interval
+	self.MESSAGE_INTERVAL = 0.50;
+
+	self.NEW_AREA = 200;
+	self.NEW_AREA_WIDTH  = (self.BASE_WIDTH / 2) + self.NEW_AREA - 250;
+	self.NEW_AREA_HEIGHT = (self.BASE_HEIGHT / 2) + self.NEW_AREA;
+
+	self.ENABLE_SHADOWS = false;
+
+	self.HEARTBEAT = 10.00;
+
+}
+new _settings();
